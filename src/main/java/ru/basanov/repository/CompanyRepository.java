6dc3545cafab488b762aba00ff2114b499358565
Mingiyan1987 <basanov.mt87@gmail.com>
2019-02-15 14:40:40 +0300

    добавил test.jsp

--- src/main/java/ru/basanov/repository/CompanyRepository.java
+++ src/main/java/ru/basanov/repository/CompanyRepository.java
@@ -2,7 +2,7 @@ package ru.basanov.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import ru.basanov.model.;
+import ru.basanov.model.Company;
 
 @Repository
 public interface CompanyRepository extends CrudRepository<Company, String> {
